fc40d345bdd347c8b4a31ade671da3126565fd4f
Akrjourney <akarikuma1993@gmail.com>
2020-10-12 17:12:11 +0900

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Join us!</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+
+    <link rel="stylesheet" href="assets/css/stylesheet.css">
+
+    <body>
+    <div class="jumbotron jumbotron-extend">
+    <div class="container-fluid jumbotron-container">
+    <h1 class="site-name">新しい仲間と繋がろう。</h1>
+    <p><a class="btn btn-black" href="#" role="button">新規ユーザー登録</a></p>
+    <p><a class="btn btn-black" href="#" role="button">ログイン</a></p>
+    <p><a class="btn btn-black" href="#" role="button">ゲストログイン</a></p>
+    </div>
+    </div>
+
+</body>
+</html>
