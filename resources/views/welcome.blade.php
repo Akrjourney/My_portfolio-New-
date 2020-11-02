1d4359d148847470dad22cbda4813a42dc3b9cc1
Akrjourney <akarikuma1993@gmail.com>
2020-11-02 03:55:57 +0000

    ゲストログイン設定

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,16 @@
     <div class="jumbotron jumbotron-extend">
     <div class="container-fluid jumbotron-container">
     <h1 class="site-name">Learn Together.</h1>
+    <div class="guest">
+    <font color ="white">
+    <ul>
+    <p>テストログイン</p>
+    <li>メールアドレス：guest@sample.com</li>
+    <li>パスワード：guestuser</li>
+    </ul>
+    </font>
+    </div>
+
     <p><a href="{{ route('register') }}" class="btn-square-slant"></i>新規ユーザー登録</a></p>
     <p><a href="{{ route('login') }}" class="btn-square-slant">ログイン</a></p>
     </div>
